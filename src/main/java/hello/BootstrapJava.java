cfbc0ce2cfb9a34c0df5d47db59a5d18d09485a5
GMLDD <lloo0331oo@126.com>
2017-09-18 23:26:46 +0800

    scala的HelloWorld部分

--- src/main/java/hello/BootstrapJava.java
+++ src/main/java/hello/BootstrapJava.java
@@ -1,15 +1,10 @@
 package hello;
 
-import hello.GreetingInScala;
-import hello.HelloScala;
-
 public class BootstrapJava {
 
     public static void main(String[] args) {
         GreetingInScala scala = new GreetingInScala();
         scala.greet();
 
-        HelloScala testScala = new HelloScala();
-        testScala.test();
     }
 }  
