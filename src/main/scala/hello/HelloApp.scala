680a3e859f31bff18045c50d438a2ee5e6827195
lloo0331 <lloo0331oo@126.com>
2017-10-09 14:01:43 +0800

    demo

---
+++ src/main/scala/hello/HelloApp.scala
@@ -0,0 +1,6 @@
+package hello
+
+//HelloWorld的最简版本
+object HelloApp extends App{
+  println("Hello World")
+}
---
+++ src/main/scala/print/PrintStarInScala.scala
@@ -0,0 +1,34 @@
+package print
+
+import utils.PlUtils
+;
+
+class PrintStarInScala {
+
+
+}
+
+object PrintStarInScala{
+
+
+  def main(args: Array[String]): Unit = {
+    (1 to 9).map("*" * _).foreach(println(_))
+
+    (1 to 9).filter(_ % 2==0)
+
+    PlUtils.pri((1 to 9).reduceLeft(_ * _))
+
+    PlUtils.pri((1 to 9).reduceRight(_ * _))
+
+    PlUtils.pri((1 to 9).reduce(_ * _))
+
+    val arr = "Mary has a little lamb".split(" ").sortWith(_.length>_.length)
+
+    PlUtils.priArr(arr)
+
+  }
+
+
+
+
+}
